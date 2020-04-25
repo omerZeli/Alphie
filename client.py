1fe9355f4d0679b771f94992c9dda400f9fea280
omerZeli <55653516+omerZeli@users.noreply.github.com>
2020-04-25 15:27:17 +0300

    Add files via upload

---
+++ client.py
@@ -0,0 +1,182 @@
+import socket
+import tkinter as tk
+
+
+class client:
+
+    def __init__(self, ip):
+        self.ip = ip    # the ip of the server
+        self.my_socket = socket.socket()    # build the socket of the client
+        self.user_name = ""
+        self.password = ""
+        self.root = tk.Tk()     # create the graphic
+        self.root.title("Alphie")   # the title of the window
+        self.root.configure(background="firebrick1")    # change the window color
+        self.text_box = tk.Text(self.root, height=10, width=150)    # create text box
+        self.entry = tk.Entry(self.root)    # create entry
+        self.enter_button = tk.Button(self.root, text="enter")      # create enter button
+        self.menu_bar = tk.Menu(self.root)
+        self.back_menu = tk.Menu(self.menu_bar, tearoff=0)
+
+    # input details in sign up/in
+    def input_sign(self, sign):
+        self.user_name = self.input("Enter user name")
+        self.password = self.input("Enter password")
+        msg = "{}; {}, {}".format(sign, self.user_name, self.password)
+        return msg
+
+    # send a message and get a receive an answer
+    def send_and_rec(self, msg):
+        msg = msg.encode()
+        self.my_socket.send(msg)
+        ans = self.my_socket.recv(1024)
+        ans = ans.decode()
+        return ans
+
+    def print_message(self, text):
+        self.text_box.insert(tk.END, text + "\n")
+
+    def input(self, text):
+        self.print_message(text)
+        var = tk.IntVar()
+        self.enter_button.pack()
+        self.enter_button.configure(command=lambda: var.set(1))
+        self.enter_button.wait_variable(var)
+        output = self.entry.get()
+        self.print_message(output)
+        self.enter_button.pack_forget()
+        self.entry.delete(0, 'end')
+        return output
+
+    # delete all the items in thw window
+    def delete_items(self):
+        lst = self.root.winfo_children()
+        for item in lst:
+            item.pack_forget()
+
+    # delete all the text in the text box
+    def delete_text_box(self):
+        self.text_box.delete('1.0', tk.END)
+
+    # send quit to the server to stop the connection with the server
+    def close_button(self):
+        self.my_socket.send("close_window".encode())
+        self.my_socket.close()
+        self.root.destroy()
+
+    def create_menu(self):
+        self.menu_bar.add_cascade(label="back", menu=self.back_menu)
+        self.root.config(menu=self.menu_bar)
+
+    # graphic - the start of the app, choose sign up/in
+    def show_start(self):
+        self.delete_items()
+        self.delete_text_box()
+        self.back_menu.delete(0)
+        self.text_box.pack()
+        self.print_message("Hello, my name is Alphie")
+        self.print_message("If you have an user click sign in, else click sign up")
+        # create sign up/in buttons
+        su_button = tk.Button(self.root, text="sign up", command=lambda: self.show_sign("sign_up"))
+        su_button.pack()
+        si_button = tk.Button(self.root, text="sign in", command=lambda: self.show_sign("sign_in"))
+        si_button.pack()
+
+    # graphic - sign up/in
+    def show_sign(self, sign):
+        self.delete_items()
+        self.delete_text_box()
+        # back goes to show start
+        self.back_menu.delete(0)
+        self.back_menu.add_command(label="start", command=lambda: self.show_start())
+        self.text_box.pack()
+        self.entry.pack()
+        ser_ans = ""
+        if sign == "sign_up":
+            while ser_ans != "You signed up":
+                msg = self.input_sign("sign_up")
+                ser_ans = self.send_and_rec(msg)
+                self.print_message(ser_ans)
+        else:
+            while ser_ans != "You signed in":
+                msg = self.input_sign("sign_in")
+                ser_ans = self.send_and_rec(msg)
+                self.print_message(ser_ans)
+        self.show_search()
+
+    # graphic - the searching
+    def show_search(self):
+        self.delete_items()
+        self.delete_text_box()
+        self.back_menu.delete(0)
+        self.back_menu.add_command(label="start", command=lambda: self.show_start())
+        self.text_box.pack()
+        self.entry.pack()
+        self.print_message("Now enter the word you want to search")
+        self.print_message("And I'll show you all the results")
+        # input the word
+        word = self.input("")
+        results = self.send_and_rec(word)
+        self.show_results(results)
+
+    # graphic - the results of the searching
+    def show_results(self, results):
+        self.delete_items()
+        self.delete_text_box()
+        self.back_menu.delete(0)
+        self.back_menu.add_command(label="search", command=lambda: self.show_search())
+        self.text_box.pack()
+        self.text_box.configure(height=10)
+        self.print_message("Click the file name that you want to open")
+        # create button for every file result
+        results_list = results.split(', ')
+        for button_text in results_list:
+            button = tk.Button(self.root, text=button_text,
+                               command=lambda name=button_text: self.show_file(results, name))
+            button.pack()
+            if button_text == "no results":
+                button.configure(command=lambda: 0)
+
+    # graphic - the file
+    def show_file(self, results, file_name):
+        self.delete_items()
+        self.delete_text_box()
+        self.back_menu.delete(0)
+        self.back_menu.add_command(label="results", command=lambda: self.show_results(results))
+        self.text_box.pack()
+        self.text_box.configure(height=30)
+        # get the file from server
+        text = self.rec_file(file_name)
+        self.print_message(text)
+
+    # get the file from server
+    def rec_file(self, file_name):
+        # ask the server for the file length
+        msg = "file_len; {}".format(file_name)
+        length = self.send_and_rec(msg)
+        length = int(length)
+        # ask the server for the file
+        msg = "send_file; {}".format(file_name)
+        self.my_socket.send(msg.encode())
+        text = ""
+        for i in range(length):
+            data = self.my_socket.recv(1024)
+            data = data.decode()
+            text += data
+        return text
+
+    def main(self):
+        # connect the client to the server
+        self.my_socket.connect((self.ip, 8820))
+        # x button
+        self.root.protocol('WM_DELETE_WINDOW', self.close_button)
+        # the size of the window
+        self.root.geometry("1500x1500")
+        self.create_menu()
+        # start the app
+        self.show_start()
+        self.root.mainloop()
+
+
+server_ip = '127.0.0.1'
+client(server_ip).main()
---
+++ create_db.py
@@ -0,0 +1,117 @@
+import sqlite3
+import os
+
+
+class create:
+
+    def __init__(self, data_base_path, files_path):
+        self.data_base_path = data_base_path
+        self.keys_table_name = "KEYS"
+        self.users_table_name = "USERS"
+        self.files_path = files_path
+
+    # create data base table
+    def create_table(self, table_path, table_name, column1, column2):
+        conn = sqlite3.connect(table_path)
+        st = '''CREATE TABLE {}
+                        (
+                        {}  TEXT    NOT NULL,
+                        {}   TEXT    NOT NULL
+                        );'''.format(table_name, column1, column2)
+        conn.execute(st)
+        conn.close()
+
+    # find all the file in the data base
+    def find_files(self, path):
+        files_list = []
+        self.open(path, files_list)
+        return files_list
+
+    # recursion
+    def open(self, path, files_list):
+        if self.empty(path):
+            files_list.append(path)
+        else:
+            for i in os.listdir(path):
+                self.open(path + "\\" + i, files_list)
+
+    def empty(self, path):
+        try:
+            os.listdir(path)
+            return False
+        except:
+            return True
+
+    # find the lines keys in the files
+    def find_keys(self, special):
+        files_list = self.find_files(self.files_path)
+        lines_list = []
+        for path in files_list:
+            try:
+                # read the current file
+                file = open(path, "r")
+                contents = file.read()
+                lines = contents.split("\n")
+                # the keys
+                general_keys = ["import"]
+                python_keys = ["import", "def"]
+                java_keys = ["import", "public"]
+                # add word that client searched
+                if special != "":
+                    keys = [special]
+                # add keys that known to me
+                elif ".py" in path:
+                    keys = python_keys
+                elif ".java" in path:
+                    keys = java_keys
+                else:
+                    keys = general_keys
+                # find the keys in the current file
+                for line in lines:
+                    for key in keys:
+                        if key in line:
+                            tup = (line, path)
+                            lines_list.append(tup)
+                # add the last name of the file
+                last_path = path.split("\\")[-1]
+                lines_list.append((last_path, path))
+                file.close()
+            except:
+                pass
+        return lines_list
+
+    # insert data to data base table
+    def insert(self, table_name, column1, column2, data):
+        conn = sqlite3.connect(self.data_base_path)
+        st = "INSERT INTO {} ({},{}) VALUES ('{}', '{}')".format(table_name, column1, column2, data[0], data[1])
+        try:
+            conn.execute(st)
+        except:
+            print("the line " + st + " can't be in the db")
+        conn.commit()
+        conn.close()
+
+    # insert the keys to the data base after they were found
+    def insert_keys(self, column1, column2, special=""):
+        lines_list = self.find_keys(special)
+        for data in lines_list:
+            self.insert(self.keys_table_name, column1, column2, data)
+
+    def main(self):
+        try:
+            keys_column1 = 'the_key'
+            keys_column2 = 'the_file'
+            users_column1 = 'user_name'
+            users_column2 = 'password'
+            self.create_table(self.data_base_path, self.users_table_name, users_column1, users_column2)
+            print("Users data base created")
+            self.create_table(self.data_base_path, self.keys_table_name, keys_column1, keys_column2)
+            self.insert_keys(keys_column1, keys_column2)
+            print("Keys data base created")
+        except:
+            pass
+
+
+data_base_path = 'data_base.db'
+files_path = 'files_data'
+create(data_base_path, files_path).main()
